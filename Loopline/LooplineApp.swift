aad407239ae5d3326d39ed8f53cdcd68fa1ab81d
Mansi-126 <mansigangani126@gmail.com>
2026-07-03 11:57:24 +0530

    Initial Commit

---
+++ Loopline/LooplineApp.swift
@@ -0,0 +1,17 @@
+//
+//  LooplineApp.swift
+//  Loopline
+//
+//  Created by Mansi Gangani on 03/07/26.
+//
+
+import SwiftUI
+
+@main
+struct LooplineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
